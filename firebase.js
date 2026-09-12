0e1abfbe8d8323400d25cbfa59ac5f68e1d4be05
jonathanmentor62-web <jonathanmentor62@gmail.com>
2026-09-12 09:30:42 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-storage.js";
-
-/*
-  JDA Networks Firebase Configuration
-*/
 
 const firebaseConfig = {
   apiKey: "AIzaSyAPnOHdVISPw_fGBLdMLELSU9f1IWMTC3I",
@@ -16,26 +11,7 @@ const firebaseConfig = {
   appId: "1:383915852673:web:4b185c0387ed0bbad1de80"
 };
 
-/*
-  Initialize Firebase
-*/
-
 const app = initializeApp(firebaseConfig);
 
-/*
-  Firebase Authentication
-*/
-
 export const auth = getAuth(app);
-
-/*
-  Cloud Firestore
-*/
-
 export const db = getFirestore(app);
-
-/*
-  Firebase Storage
-*/
-
-export const storage = getStorage(app);

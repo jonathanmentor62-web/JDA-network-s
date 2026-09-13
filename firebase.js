784d7fd9a615a19c2072519102f343d35ec390a1
jonathanmentor62-web <jonathanmentor62@gmail.com>
2026-09-13 22:21:57 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,21 @@
+// =========================================================
+// jChat Firebase
+// =========================================================
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+import {
+  getStorage
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-storage.js";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyAPnOHdVISPw_fGBLdMLELSU9f1IWMTC3I",
@@ -11,7 +26,9 @@ const firebaseConfig = {
   appId: "1:383915852673:web:4b185c0387ed0bbad1de80"
 };
 
+
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
